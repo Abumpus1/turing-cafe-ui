2acf15a9d96177dcd567246420582b21218f2439
Alex Bumpus <alexander.bumpus@yahoo.com>
2022-05-25 10:49:48 -0600

    Setup form layout

--- src/Form.js
+++ src/Form.js
@@ -5,14 +5,21 @@ class Form extends Component {
   constructor() {
     super()
     this.state = {
-
+      name: "",
+      date: "",
+      time: "",
+      number: null
     }
   }
 
   render() {
     return (
       <div className='resy-form'>
-        <h2>FORM</h2>
+        <input name="name" type="text" value={this.state.name} placeholder="Name..." />
+        <input name="date" type="text" value={this.state.date} placeholder="Date (mm/dd)" />
+        <input name="time" type="text" value={this.state.time} placeholder="Time" />
+        <input name="number" type="number" value={this.state.number} placeholder="Number of guests" />
+        <button>Make Reservation</button>
       </div>
     )
   }
